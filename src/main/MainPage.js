9f43baaabcc9173cb756c19e0b18d34d8b0225f0
Helena Hope-Carter <h.hopecarter@gmail.com>
2022-04-16 18:55:14 +0100

    first commit

---
+++ src/main/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar';
+import "./main.css";
+
+function mainPage() {
+  return (
+    <div className="mainPage">
+      <h1>Jei Ganiyeva</h1>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default mainPage;
---
+++ src/main/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <button>Portraits</button>
+        <button>Film</button>
+        <button>Other</button>
+        <button>Projects</button>
+        <button>Bio</button>
+        <button>Blog</button>
+    </div>
+  )
+}
+
+export default Navbar
